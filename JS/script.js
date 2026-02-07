a73aa25c6a9cd4d02a9f0128a6bb1a2fd90f447c
SVCCaterers <Svrcaterersservice@gmail.com>
2026-02-07 19:06:29 +0530

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -3,11 +3,12 @@ document.addEventListener("DOMContentLoaded", function() {
   const logo = document.getElementById("rotating-logo");
   if (!logo) return;
 
-  const logos = ["images/logo1.png", "images/logo2.png"];
+  const logos = ["images/logo1.jpeg", "images/logo2.jpeg"];
   let index = 0;
 
   setInterval(() => {
     index = (index + 1) % logos.length;
     logo.src = logos[index];
   }, 5000); // switch every 5 seconds
+
 });
